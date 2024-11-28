fe44ab6d71bc3c9a5ef907899bbabf9733ff95fa
Kristina Lingthep <kris.lingthep@gmail.com>
2024-11-28 18:12:16 +0300

    добавляет price и табы на js

---
+++ source/js/price-tabs.js
@@ -0,0 +1,26 @@
+const showTabs = (tabButton) => {
+  const tabs = tabButton.closest('.price__tabs');
+  if (tabButton.classList.contains('price__button--active')) {
+    return;
+  }
+
+  const targetId = tabButton.dataset.targetId;
+  const tabList = tabs.querySelector(`.price__options[data-id="${targetId}"]`);
+
+  if (tabList) {
+    const tabButtonActive = tabs.querySelector('.price__button--active');
+    tabButtonActive.classList.remove('price__button--active');
+    const tabShow = tabs.querySelector('.price__options--show');
+    tabShow.classList.remove('price__options--show');
+    tabButton.classList.add('price__button--active');
+    tabList.classList.add('price__options--show');
+  }
+};
+
+document.addEventListener('click', (evt) => {
+  if (evt.target && !evt.target.closest('.price__button')) {
+    return;
+  }
+  const tabButton = evt.target.closest('.price__button');
+  showTabs(tabButton);
+});
